a553318d040fbb1be9a680ab146e0c8d2b33c320
hikmatillayev <hikmatillayevoktam24@gmail.com>
2023-05-16 15:58:08 +0500

    routing added

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,15 @@
 import { Box } from "@mui/material"
 import { Route, Routes } from "react-router-dom"
-import{Main, Channel} from "../"
+import { Main, Channel, VideoDetail, Search } from "../"
 
 const App = () => {
   return (
     <Box>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/channel" element={<Channel />} />
+        <Route path="/channel/:id" element={<Channel />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/search/:id" element={<Search />} />
       </Routes>
     </Box>
   )
