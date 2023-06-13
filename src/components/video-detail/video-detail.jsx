ca49c4c1e3b2f47d94a130eee412c8a53d76cde7
hikmatillayev <hikmatillayevoktam24@gmail.com>
2023-06-13 19:25:37 +0500

    chanched

--- src/components/video-detail/video-detail.jsx
+++ src/components/video-detail/video-detail.jsx
@@ -5,9 +5,12 @@ import ReactPlayer from 'react-player'
 import { Box, Chip, Stack, Typography } from "@mui/material"
 import { FavoriteOutlined, MarkChatRead, Tag, Visibility } from "@mui/icons-material"
 import renderHTML from 'react-render-html'
+import Loader from "../loader/loader"
+import { Videos } from '../'
 
 const VideoDetail = () => {
     const [videoDeatil, setVideoDetail] = useState(null)
+    const [relatedVideo, setRelatedVideo] = useState([])
     const { id } = useParams()
 
     useEffect(() => {
@@ -15,7 +18,10 @@ const VideoDetail = () => {
             try {
                 const data = await ApiService.fetching(`videos?part=snippet, statistics&id=${id}`)
                 setVideoDetail(data.items[0])
-                console.log(data.items[0])
+                const relatedData = await ApiService.fetching(
+                    `search?part=snippet&relatedToVideoId=${id}&type=video`
+                )
+                setRelatedVideo(relatedData.items)
             } catch (error) {
                 console.log(error)
             }
@@ -24,6 +30,8 @@ const VideoDetail = () => {
         getData()
     }, [id])
 
+    if (!videoDeatil?.snippet) return <Loader />
+
     // const {
     //     snippet: { title, channelId, channelTitle, description, tags, thumnails },
     //     statistics: { viewCount, likeCount, commentCount }
@@ -31,8 +39,8 @@ const VideoDetail = () => {
 
     return (
         <Box minHeight={'90vh'} mb={10}>
-            <Box display={'flex'}>
-                <Box width={'75%'}>
+            <Box display={'flex'} sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
+                <Box width={{ xs: '100%', md: '75%' }}>
                     <ReactPlayer
                         url={`https://www.youtube.com/watch?v=${id}`}
                         className='react-player'
@@ -63,15 +71,25 @@ const VideoDetail = () => {
                         </Stack>
                         <Stack sx={{ opacity: 0.7 }} direction='row' alignItems='center' gap='3px'>
                             <FavoriteOutlined />
-                            {parseInt(videoDeatil.statistics.viewCount).toLocaleString()} views
+                            {parseInt(videoDeatil.statistics.likeCount).toLocaleString()} likes
                         </Stack>
                         <Stack sx={{ opacity: 0.7 }} direction='row' alignItems='center' gap='3px'>
                             <MarkChatRead />
-                            {parseInt(videoDeatil.statistics.viewCount).toLocaleString()} views
+                            {parseInt(videoDeatil.statistics.commentCount).toLocaleString()} comment
                         </Stack>
                     </Stack>
                 </Box>
-                <Box width={'25%'}>Suggested video</Box>
+                <Box
+                    width={{ xs: '100%', md: '25%' }}
+                    px={2}
+                    py={{ md: 1, xs: 5 }}
+                    justifyContent='center'
+                    alignItems='center'
+                    overflow={'scroll'}
+                    maxHeight={'100vh'}
+                >
+                    <Videos videos={relatedVideo} />
+                </Box>
             </Box>
         </Box>
     )
